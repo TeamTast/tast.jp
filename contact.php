6c41515658ad222f5aafab9145e23d0d497eaf29
oiudon <more.more.udon@gmail.com>
2022-01-15 00:21:39 +0900

    コードきれいにするのとCONTACTがWORKS問題

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WORKS | Tast</title>
+    <title>CONTACT | Tast</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/contact.css">
     <link rel="shortcut icon" href="imgs/favicon.ico" type="image/x-icon">
